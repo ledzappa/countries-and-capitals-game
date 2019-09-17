2e315cdcf4f5a7d83ab12341a47fbbb2cc4b2781
Mikael Juntti <juntti.mikael@gmail.com>
2019-09-17 22:25:01 +0200

    basic game functionality

--- js/script.js
+++ js/script.js
@@ -1,17 +1,74 @@
-const merge = [];
+let countries, questionNum, correctAnswers;
 
 let res = countriesByCapital.map(x =>
   Object.assign(x, {
-    continent: countriesByContinent.find(y => y.country === x.country)
-      .continent
+    continent: countriesByContinent.find(y => y.country === x.country).continent
   })
 );
 
 function start() {
+  resetState();
   let continent = document.getElementById("continent-select").value;
 
   if (continent.length > 0) {
-    console.log(res.filter(obj => obj.continent === continent));
-    console.log(res);
+    countries = res.filter(obj => obj.continent === continent);
+  } else {
+    countries = res;
   }
+
+  nextQuestion();
+  getAlternatives(countries[0].city);
+}
+
+function resetState() {
+  countries = [];
+  questionNum = 0;
+  correctAnswers = 0;
+}
+
+function nextQuestion() {
+  document.getElementById("question").innerHTML =
+    questionNum + ". What's the capital in " + countries[questionNum].country;
+  getAlternatives();
+}
+
+function getAlternatives() {
+  let alternatives = [];
+  while (alternatives.length < 3) {
+    let randomItem = Math.floor(Math.random() * countries.length);
+
+    if (alternatives.indexOf(countries[randomItem].city) === -1) {
+      alternatives.push(countries[randomItem].city);
+    }
+  }
+
+  // insert right answer in random position
+  alternatives.splice(
+    Math.floor(Math.random() * (alternatives.length + 1)),
+    0,
+    countries[questionNum].city
+  );
+
+  let html = "";
+  alternatives.forEach(alternative => {
+    html +=
+      "<button onclick=\"checkAnswer('" +
+      alternative +
+      "')\">" +
+      alternative +
+      "</button>";
+  });
+
+  document.getElementById("alternatives").innerHTML = html;
+}
+
+function checkAnswer(answer) {
+  if (answer == countries[questionNum].city) {
+    correctAnswers++;
+  }
+
+  document.getElementById("correct-answers").innerHTML =
+    "Correct answers: " + correctAnswers + " / " + countries.length;
+  questionNum++;
+  nextQuestion();
 }
