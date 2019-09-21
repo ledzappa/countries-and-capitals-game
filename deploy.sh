6eb8406e3013f1e1847e26d6164ecba5403fba00
Mikael Juntti <juntti.mikael@gmail.com>
2019-09-21 18:02:57 +0200

    changed background color

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-rm -rf ./docs
-mkdir ./docs
-rsync -av --progress . ./docs --exclude ./docs
-rm -rf ./docs/package.json
-rm -rf ./docs/package-lock.json
-rm -rf ./docs/docs
-rm -rf ./docs/deploy.sh
-rm -rf ./docs/.vscode
-rm -rf ./docs/README.md 
-rm -rf ./docs/.gitignore
-rm -rf ./docs/_config.yml
+git checkout gh-pages
+git merge master
